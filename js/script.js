fadacacbe4cab82bd880d8b464531e5a4ac71cab
TayunIxcoy <44215963+TayunIxcoy@users.noreply.github.com>
2019-10-21 18:35:22 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
 
+$(".talk").click(function() {
+
+
+);
 //GLOBAL VARIABLES
 /* global $ */
 
